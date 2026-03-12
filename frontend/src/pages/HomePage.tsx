b80806ddf44fbdd052d823414c48bbb63f047296
Yassir aittata <yassir.aittata@gmail.com>
2026-03-12 15:05:43 +0000

    make home page responsive

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,10 +2,12 @@ const HomePage = () => {
   return (
     <div className="flex flex-col gap-12">
       <div className="-mt-16 flex flex-col gap-5 rounded-lg bg-white py-8 text-center shadow-md">
-        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+        <h1 className="font-bold tracking-tight text-orange-600 sm:text-xl md:text-3xl lg:text-5xl">
           Tuck into a takeway today
         </h1>
-        <span className="text-xl">Food is just a click away</span>
+        <span className="text-sm sm:text-lg md:text-xl">
+          Food is just a click away
+        </span>
       </div>
     </div>
   )
