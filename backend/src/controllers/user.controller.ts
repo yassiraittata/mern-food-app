fb783c57aad23e836bceea5e763c2dd5ca44c138
Yassir aittata <yassir.aittata@gmail.com>
2026-04-16 17:07:23 +0100

    create user

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,1 +1,33 @@
-export const createUser = async (req, res) => {};
+import type { RequestHandler } from "express";
+import createError from "http-errors";
+
+import User from "../models/user.ts";
+
+type CreateUserRequestBody = {
+  auth0Id: string;
+  email: string;
+  name?: string;
+  addressLine1?: string;
+  city?: string;
+  country?: string;
+};
+
+export const createUser: RequestHandler<
+  unknown,
+  unknown,
+  CreateUserRequestBody,
+  unknown
+> = async (req, res, next) => {
+  const { auth0Id, email } = req.body;
+
+  const existingUser = await User.findOne({ auth0Id });
+  if (existingUser) {
+    // next(createError(400, "User already exists"));
+    return res.status(200).json(existingUser);
+  }
+
+  const newUser = new User(req.body);
+  await newUser.save();
+
+  res.status(201).json(newUser.toObject());
+};
