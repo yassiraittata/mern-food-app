2db4f1c325e074f623b4a9c1f53a5a9a5b4c477f
Yassir aittata <yassir.aittata@gmail.com>
2026-04-16 15:29:18 +0100

    cretae drop down in mobile menu

--- frontend/src/components/MobbileNav.tsx
+++ frontend/src/components/MobbileNav.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "lucide-react"
+import { CircleUserRound, Menu } from "lucide-react"
 import {
   Sheet,
   SheetContent,
@@ -8,8 +8,12 @@ import {
 } from "./ui/sheet"
 import { Separator } from "./ui/separator"
 import { Button } from "./ui/button"
+import { useAuth0 } from "@auth0/auth0-react"
+import MobileNavLinks from "./MobileNavLinks"
 
-const MobbileNav = () => {
+const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0()
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -17,17 +21,31 @@ const MobbileNav = () => {
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          <span>Welcome to Foodie.com!</span>
+          {isAuthenticated ? (
+            <span className="flex items-center gap-2 font-bold">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span> Welcome to MernEats.com!</span>
+          )}
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 bg-orange-500 font-bold" variant="default">
-            Log in
-          </Button>
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 bg-orange-500 font-bold"
+            >
+              Log In
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
   )
 }
 
-export default MobbileNav
+export default MobileNav
--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -20,12 +20,12 @@ const UsernameMenu = () => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-neutral-primary-medium border-default-medium rounded-base text-body z-10 w-52 overflow-hidden border bg-white p-2 text-center text-sm font-medium shadow-lg">
         <DropdownMenuItem>
-          <p className="hover:bg-neutral-tertiary-medium h inline-flex w-full items-center rounded p-2">
+          <p className="inline-flex w-full items-center rounded p-2">
             {user?.email}
           </p>
         </DropdownMenuItem>
         <Separator />
-        <DropdownMenuItem className="text-center hover:bg-gray-200">
+        <DropdownMenuItem className="mt-1 text-center hover:bg-gray-200">
           <Link
             to="/user-profile"
             className="hover:bg-neutral-tertiary-medium hover:text-heading inline-flex w-full items-center rounded p-2"
@@ -36,19 +36,11 @@ const UsernameMenu = () => {
             Profile
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem className="flex w-full items-center justify-center text-center">
-          {/* <Link
-            to="/user-profile"
-            className="hover:bg-neutral-tertiary-medium hover:text-heading inline-flex w-full items-center rounded p-2"
-          >
-            <span>
-              <UserRound className="h-4 text-gray-500" />
-            </span>
-            Profile
-          </Link> */}
+        <DropdownMenuItem className="flex w-full">
           <Button
-          
-          onClick={() => logout()} className="text-body border-default hover:text-heading inline-flex cursor-pointer items-center rounded-sm border bg-white px-4 py-2.5 leading-5 font-medium shadow-xs hover:border-orange-300 hover:bg-orange-300 focus:outline-none">
+            onClick={() => logout()}
+            className="text-body border-default hover:text-heading inline-flex w-full cursor-pointer items-center rounded-sm border bg-white px-4 py-2.5 leading-5 font-medium shadow-xs hover:border-orange-300 hover:bg-orange-300 focus:outline-none"
+          >
             <LogOutIcon />
             Logout
           </Button>
